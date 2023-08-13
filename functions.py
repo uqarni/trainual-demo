f3b50ce987bc24b9ffba0401b636671617083bac
uqarni <108438834+uqarni@users.noreply.github.com>
2023-08-13 12:31:35 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,7 +14,7 @@ def random_date():
   formatted_date = random_date.strftime("%Y-%m-%d")
   return formatted_date  # Outputs the random date in "YYYY-MM-DD" format
 
-# You can return the formatted_date from here
+
 
 #generate openai response; returns messages with openai response
 def ideator(messages):
@@ -71,38 +71,4 @@ def ideator(messages):
   return messages, count
 
 
-#prompt user with botresponse in terminal and ask for an input. returns messages with human response
-#change this from input function to streamlit function
-def terminaltalker(messages):
 
-  botresponse = messages[-1]["content"]
-  userresponse = input(botresponse+"\n")
-  messages.append(
-  {
-    "role": "user",
-    "content": userresponse
-  }
-  )
-  return messages
-
-#starts terminal conversation. Respond with exit() to exit. 
-def terminalbot():
-    import redis
-    redis_host = os.environ.get("REDIS_1_HOST")
-    redis_port = 25061
-    redis_password = os.environ.get("REDIS_1_PASSWORD")
-    rd = redis.Redis(host=redis_host, port=redis_port, password=redis_password, ssl=True, ssl_ca_certs="/etc/ssl/certs/ca-certificates.crt")
-
-    system_prompt = rd.get("seiji@trainual.com-systemprompt-01").decode('utf-8')
-    initial_text = rd.get("seiji@trainual.com-initialtext-01").decode('utf-8')
-    
-    #initialize message
-    messages = [
-          {"role": "system", "content": system_prompt},
-          {"role": "assistant", "content": initial_text}]
-    
-    while True:
-       messages = terminaltalker(messages)
-       if messages[-1]["content"] == "exit()":
-          break
-       ideator(messages)
